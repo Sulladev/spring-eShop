e110e6b189b2346e89d3dfb72ce758dddb3dc02f
sulla <inikolazzz@gmail.com>
2020-12-26 15:31:21 +0300

    finished with adding product and category entities and repositories

---
+++ geek-study-eshop/shop-database/src/main/java/ru/geekbrains/persist/model/Product.java
@@ -0,0 +1,77 @@
+package ru.geekbrains.persist.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name", length = 32, nullable = false)
+    private String name;
+
+    @Column(name = "image")
+    private String image;
+
+    @Column(name = "price")
+    private Double price;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    public Product() {
+    }
+
+    public Product(Long id, String name, String image, Double price, Category category) {
+        this.id = id;
+        this.name = name;
+        this.image = image;
+        this.price = price;
+        this.category = category;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+}
