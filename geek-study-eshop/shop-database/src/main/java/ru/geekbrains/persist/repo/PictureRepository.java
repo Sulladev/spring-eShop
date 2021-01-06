238b1676c18ea967c41904397acab4d15845bffb
sulla <inikolazzz@gmail.com>
2021-01-06 19:24:37 +0300

    started jpql methods

--- geek-study-eshop/shop-database/src/main/java/ru/geekbrains/persist/repo/PictureRepository.java
+++ geek-study-eshop/shop-database/src/main/java/ru/geekbrains/persist/repo/PictureRepository.java
@@ -1,7 +1,14 @@
 package ru.geekbrains.persist.repo;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.quartz.QuartzDataSource;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.geekbrains.persist.model.Picture;
 
 public interface PictureRepository extends JpaRepository<Picture, Long> {
+
+    @Query ("SELECT p FROM Picture p JOIN p.pictureData pD WHERE pD.data  IS NOT NULL ")
+    String findById ();
 }
--- geek-study-eshop/shop-picture-service/src/main/java/ru/geekbrains/service/PictureServiceBlobImpl.java
+++ geek-study-eshop/shop-picture-service/src/main/java/ru/geekbrains/service/PictureServiceBlobImpl.java
@@ -1,6 +1,7 @@
 package ru.geekbrains.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
 import ru.geekbrains.persist.model.Picture;
 import ru.geekbrains.persist.model.PictureData;
@@ -9,6 +10,7 @@ import ru.geekbrains.persist.repo.PictureRepository;
 import java.util.Optional;
 
 @Service
+@ConditionalOnProperty(name = "picture.storage.type", havingValue = "database")
 public class PictureServiceBlobImpl implements PictureService {
 
     private final PictureRepository repository;
@@ -21,12 +23,16 @@ public class PictureServiceBlobImpl implements PictureService {
     @Override
     public Optional<String> getPictureContentTypeById(long id) {
         return repository.findById(id)
+                // TODO перенести проверку на уровень JPQL запроса
+                .filter(pic -> pic.getPictureData().getData() != null)
                 .map(Picture::getContentType);
     }
 
     @Override
     public Optional<byte[]> getPictureDataById(long id) {
         return repository.findById(id)
+                // TODO перенести проверку на уровень JPQL запроса
+                .filter(pic -> pic.getPictureData().getData() != null)
                 .map(pic -> pic.getPictureData().getData());
     }
 
