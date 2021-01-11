9d919885304e9f9093109cc62b7dc49acb8a1289
sulla <inikolazzz@gmail.com>
2021-01-11 18:01:01 +0300

    working on product_details.html and cart.html

--- geek-study-eshop/shop-ui/src/main/java/ru/geekbrains/service/ProductService.java
+++ geek-study-eshop/shop-ui/src/main/java/ru/geekbrains/service/ProductService.java
@@ -2,10 +2,12 @@ package ru.geekbrains.service;
 
 import ru.geekbrains.controller.repr.ProductRepr;
 
+import java.io.Serializable;
 import java.util.List;
 import java.util.Optional;
 
-public interface ProductService {
+public interface ProductService extends Serializable {
+
     List<ProductRepr> findAll();
 
     Optional<ProductRepr> findById(Long id);
