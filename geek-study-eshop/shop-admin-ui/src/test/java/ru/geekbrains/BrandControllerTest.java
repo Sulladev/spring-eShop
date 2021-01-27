46ba3bbc1fc06b887c6c6f703863b9ef42667993
sulla <inikolazzz@gmail.com>
2021-01-27 11:20:45 +0300

    working on BrandControllerTest and CategoriesControllerTest

--- geek-study-eshop/shop-admin-ui/src/test/java/ru/geekbrains/BrandControllerTest.java
+++ geek-study-eshop/shop-admin-ui/src/test/java/ru/geekbrains/BrandControllerTest.java
@@ -11,17 +11,25 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import ru.geekbrains.controller.repr.BrandRepr;
 import ru.geekbrains.persist.model.Brand;
+import ru.geekbrains.persist.model.Category;
 import ru.geekbrains.persist.repo.BrandRepository;
+import ru.geekbrains.service.BrandService;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ActiveProfiles("test")
 @TestPropertySource(locations = "classpath:application-test.properties")
@@ -34,6 +42,9 @@ public class BrandControllerTest {
 
     @Autowired
     private BrandRepository brandRepository;
+    
+    @Autowired
+    private BrandService brandService;
 
     @BeforeEach
     public void init() {
@@ -57,4 +68,21 @@ public class BrandControllerTest {
         assertTrue(opt.isPresent());
         assertEquals("New brand", opt.get().getName());
     }
+
+    @WithMockUser(value = "admin", password = "admin", roles = {"ADMIN"})
+    @Test
+    public void testBrandList() throws Exception {
+        List<Brand> brands = new ArrayList<>();
+        brands.add(new Brand());
+        brands.add(new Brand());
+        
+        when(brandService.findAll()).thenReturn((List) brands);
+
+        mvc.perform(get("/brands"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("brands"))
+                .andExpect(model().attribute("brands", hasSize(2)));
+    }
+
+
 }
