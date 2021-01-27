fe1ddd28c9f02bcc3055358adda5ae3b0fc568af
sulla <inikolazzz@gmail.com>
2021-01-27 20:50:55 +0300

    working on BrandControllerTest, CategoriesControllerTest, ProductsControllerTest

--- geek-study-eshop/rabbit-mq-messenger/src/test/java/ru/geekbrains/RabbitMqMessengerApplicationTests.java
+++ geek-study-eshop/rabbit-mq-messenger/src/test/java/ru/geekbrains/RabbitMqMessengerApplicationTests.java
@@ -1,7 +1,7 @@
 package ru.geekbrains;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+        import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RabbitMqMessengerApplicationTests {
